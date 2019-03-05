f2f622149b871eb3045f5335285431ded573097b
bastien-joffre <bst.joffre@gmail.com>
2019-03-05 19:42:27 +0100

    Il manquait ça...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { PostListComponentComponent } from './post-list-component/post-list-component.component';
 import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
 import { PostService } from './services/post.service';
+import { ContainerViewComponent } from './container-view/container-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponentComponent,
-    PostListItemComponentComponent
+    PostListItemComponentComponent,
+    ContainerViewComponent
   ],
   imports: [
     BrowserModule
